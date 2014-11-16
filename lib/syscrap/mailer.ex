c1e75fff8fc475fc2c9e36d1bdbcd82ac70bf7d3
Rubén Caro <ruben.caro.estevez@gmail.com>
2014-11-16 12:42:34 +0100

    Get a usable interface to gen_smtp

--- lib/syscrap/mailer.ex
+++ lib/syscrap/mailer.ex
@@ -1,30 +1,30 @@
 defmodule Syscrap.Mailer do
-  @moduledoc """
-    Send email.
-
-    ## Example:
-      Syscrap.Mailer.send_mail("recipient@email.com", "some subject", "some body")
-  """
 
   @doc """
-    Sends an email using some sensible defaults
-    It will get smpt settings from `config/private_config.exs`
+
+    Sends given raw body to given `to` address as given `from` address.
+
+    Body is raw means it's the actual email payload, including desired headers.
+    Is up to the sender to provide a suitable body to send.
+
+    Example of use:
+
+    ```
+      raw_body = "Subject: crappy subject\r\n" <>
+                  "From: crappy@syscrap.com\r\n" <>
+                  "To: crappier@syscrap.com\r\n" <>
+                  "\r\n" <>
+                  "Actual body"
+
+      raw_body |> send from: 'crappy@syscrap.com', to: 'crappier@syscrap.com'
+    ```
   """
-  def send_mail(to \\ nil, subject \\ '[Syscrap] email', body \\ 'default super useful body') do
-    address = Application.get_env(:syscrap, :email_settings)[:address]
-    username = Application.get_env(:syscrap, :email_settings)[:username]
-    password = Application.get_env(:syscrap, :email_settings)[:password]
-    port = Application.get_env(:syscrap, :email_settings)[:port]
-    default_to = Application.get_env(:syscrap, :email_settings)[:default_to]
-    to = to || default_to
 
-    # Erlang needs strings to be charlists:
-    to = to_char_list to
-    subject = to_char_list subject
-    body = to_char_list body
+  def send(body, [from: from, to: to]) do
+    opts = Application.get_env(:syscrap, :smtp_opts)
+    email = { to, [from], body }
 
-    :application.start(:gen_smtpc)
-    options = [{:host, address}, {:port, port}]
-    :gen_smtpc.send({username, password}, to, subject, body, options)
+    :gen_smtp_client.send_blocking(email,opts)
   end
+
 end
