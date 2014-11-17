2d9c49449cf5ff5b3d9e34983fa11e3c217d7d87
Rubén Caro <ruben.caro.estevez@gmail.com>
2014-11-17 19:23:26 +0100

    Add Aggregator.Metric

--- lib/syscrap/aggregator/metric.ex
+++ lib/syscrap/aggregator/metric.ex
@@ -5,7 +5,7 @@ defmodule Syscrap.Aggregator.Metric do
     Process responsible for gathering a specific metric for the given `Target`.
   """
 
-  def start_link(opts) do: GenServer.start_link(__MODULE__, opts)
+  def start_link(opts), do: GenServer.start_link(__MODULE__, opts)
 
   def init(opts) do
 
