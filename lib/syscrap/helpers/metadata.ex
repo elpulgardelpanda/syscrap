160854b5f5c9fdd08a1e60684b519f967b90548f
Rubén Caro <ruben.caro.estevez@gmail.com>
2016-02-22 20:40:00 +0100

    Add add_in helper

--- lib/syscrap/helpers/metadata.ex
+++ lib/syscrap/helpers/metadata.ex
@@ -1,5 +1,5 @@
 alias Syscrap.Helpers.Metadata.Agent, as: MA
-alias Syscrap.Helpers, as: H
+require Syscrap.Helpers, as: H
 
 defmodule Syscrap.Helpers.Metadata do
 
@@ -7,56 +7,56 @@ defmodule Syscrap.Helpers.Metadata do
     Generic metadata helpers for saving and retrieving auxiliary state
     (supposed to be supported on maps).
 
-    Every get/set function will call `init/0`, unless the `:no_init` option is given.
+    Every get/set function will call `start/0`, unless the `:no_start` option is given.
   """
 
   @doc """
-    Update/insert given key/value into mock_agent's data
+    Update/insert given key/value into metadata agent's data
   """
   def set(v, k, opts \\ [])
   def set(func, key, opts) when is_function(func) do
-    unless opts[:no_init], do: :ok = init
+    init(opts)
     Agent.update MA, fn(v)-> Map.update(v, key, func.(nil), func) end
   end
   def set(value, key, opts), do: set(fn(_)-> value end, key, opts)
 
   @doc """
-    Update/insert given nested key/value into mock_agent's data
+    Update/insert given nested key/value into metadata agent's data
   """
   def set_in(value, keys, opts \\ []) do
-    unless opts[:no_init], do: :ok = init
+    init(opts)
     Agent.update(MA, &H.set_in(&1, value, keys))
   end
 
   @doc """
-    Update/insert given key/value into mock_agent's data, no waiting
+    Update/insert given key/value into metadata agent's data, no waiting
   """
   def cast(value, key, opts \\ []) do
-    unless opts[:no_init], do: :ok = init
+    init(opts)
     Agent.cast MA, &Map.update(&1,key,value,fn(_) -> value end)
   end
 
   @doc """
-    Update/insert given nested key/value into mock_agent's data, no waiting
+    Update/insert given nested key/value into metadata agent's data, no waiting
   """
   def cast_in(value, keys, opts \\ []) do
-    unless opts[:no_init], do: :ok = init
+    init(opts)
     Agent.cast(MA, &H.set_in(&1, value, keys))
   end
 
   @doc """
-    Get requested key from mock_agent's data. Given `default` if not found.
+    Get requested key from metadata agent's data. Given `default` if not found.
   """
   def at(key, default \\ nil, opts \\ []) do
-    unless opts[:no_init], do: :ok = init
+    init(opts)
     Agent.get(MA, &(&1)) |> Map.get(key, default)
   end
 
   @doc """
-    Get requested nested key from mock_agent's data. Given `default` if not found.
+    Get requested nested key from metadata agent's data. Given `default` if not found.
   """
   def at_in(keys, default \\ nil, opts \\ []) do
-    unless opts[:no_init], do: :ok = init
+    init(opts)
     case Agent.get(MA, &(&1)) |> get_in(keys) do
       nil -> default
       v -> v
@@ -64,16 +64,29 @@ defmodule Syscrap.Helpers.Metadata do
   end
 
   @doc """
+    Adds given element to a list on given coordinate.
+    If any intermediate element is it is created as an empty map.
+    If the final element is not a List, then it's wrapped into one (see List.wrap/1).
+  """
+  def add_in(elem, keys, opts \\ []) do
+    set_in(&(List.wrap(&1) ++ [elem]), keys, opts)
+  end
+
+  @doc """
   Start the metadata agent if it's not already started.
-  Only useful when `:no_init` is used on the get/set functions.
+  Only useful when `:no_start` is used on the get/set functions.
 
   Returns `:ok`, or `{:error, reason}`
   """
-  def init do
+  def start do
     case Agent.start(fn-> %{} end, name: MA) do
       {:ok, _} -> :ok
       {:error, {:already_started, _}} -> :ok
       other -> other
     end
+  end
+
+  defp init(opts) do
+    unless opts[:no_start], do: :ok = start
   end
 end
--- lib/syscrap/mocks/ssh.ex
+++ lib/syscrap/mocks/ssh.ex
@@ -2,7 +2,7 @@ alias Syscrap.Helpers.Metadata, as: HM
 
 defmodule Syscrap.Mocks.SSHExAllOK do
   def connect(any) do
-    HM.set_in( &(List.wrap(&1) ++ [%{args: any}]), [:test, SSHExAllOK, :connect])
+    %{args: any} |> HM.add_in([:test, SSHExAllOK, :connect])
     {:ok, :bogus}
   end
 end
